a7c6f9c677ed9de484a1534bca52f6d54caf385a
Kacper Wojak <kacperwojak17@gmail.com>
2024-04-17 19:38:35 +0200

    Main: Update to pnpm

--- src/app/__test__/index.test.tsx
+++ src/app/__test__/index.test.tsx
@@ -1,14 +1,13 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+
 import Home from '../page';
 
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />);
 
-    const heading = screen.getByText(
-      'Instantly deploy your Next.js site to a shareable URL with Vercel.',
-    );
+    const heading = screen.getByText('test');
 
     expect(heading).toBeInTheDocument();
   });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <div className='h-screen w-screen overflow-hidden bg-gray-950'>test</div>
+    <div className='h-screen w-screen overflow-hidden bg-gray-950'>
+      <div className='flex h-full flex-col items-center justify-center'>
+        <div className='text-4xl font-bold text-white'>test</div>
+      </div>
+    </div>
   );
 }
