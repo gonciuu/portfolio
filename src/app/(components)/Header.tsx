7b7203ca7602176f05c5f34e737d1d38057f852e
Kacper Wojak <kacperwojak17@gmail.com>
2024-06-09 10:13:37 +0200

    HomePage: Add basic animation

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import React from 'react';
 import { TbArrowRight } from 'react-icons/tb';
 
@@ -8,32 +11,73 @@ import FlexDiv from '../components/FlexDiv';
 const Header = () => {
   return (
     <div className='py-32'>
-      <h1>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -30 }}
+        transition={{
+          duration: 0.7,
+          type: 'spring',
+          bounce: 0.6,
+        }}
+        viewport={{ once: true, amount: 0.5 }}
+      >
         Crafting Mobile <br /> Apps with Flutter
-      </h1>
-      <h2 className='mt-4'>
+      </motion.h1>
+      <motion.h2
+        className='mt-4'
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 40 }}
+        transition={{ duration: 0.7, type: 'spring', bounce: 0.6 }}
+        viewport={{ once: true, amount: 0.5 }}
+      >
         Dive into my journey as a Flutter mobile developer,
         <br /> showcasing projects that blend innovative design
         <br /> with seamless functionality.
-      </h2>
+      </motion.h2>
       <FlexDiv className='mt-14 w-full items-center justify-between'>
         <FlexDiv className='gap-4'>
-          <Button>Explore portfolio</Button>
-          <Button
-            variant='outlined'
-            className='group'
-            icon={
-              <TbArrowRight className='-rotate-45 text-2xl duration-200 group-hover:rotate-0' />
-            }
+          <motion.div
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 20 }}
+            transition={{
+              duration: 0.7,
+              type: 'spring',
+              bounce: 0.6,
+              delay: 0.2,
+            }}
+            viewport={{ once: true, amount: 0.5 }}
           >
-            Let&apos;s talk
-          </Button>
+            <Button>Explore portfolio</Button>
+          </motion.div>
+          <motion.div
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 20 }}
+            transition={{
+              duration: 0.7,
+              type: 'spring',
+              bounce: 0.6,
+              delay: 0.4,
+            }}
+            viewport={{ once: true, amount: 0.5 }}
+          >
+            <Button
+              variant='outlined'
+              className='group'
+              icon={
+                <TbArrowRight className='-rotate-45 text-2xl duration-200 group-hover:rotate-0' />
+              }
+            >
+              Let&apos;s talk
+            </Button>
+          </motion.div>
         </FlexDiv>
-        <FlexDiv className='flex-col items-end justify-end gap-0'>
-          <span>flutter developer · designer</span>
-          <AnimatedText className='text-md mt-1 h-5 text-end' href='#projects'>
-            Scroll to explore
+        <FlexDiv className='flex-col items-end justify-end gap-1'>
+          <AnimatedText className='text-md mt-1 h-5 text-end'>
+            flutter developer · designer
           </AnimatedText>
+          <AnimatedText className='text-md mt-1 h-5 text-end'>
+            Scroll to explore
+          </AnimatedText>{' '}
         </FlexDiv>
       </FlexDiv>
     </div>
--- src/app/(components)/Projects.tsx
+++ src/app/(components)/Projects.tsx
@@ -49,35 +49,34 @@ function ProjectCard({ project }: ProjectCardProps) {
       id='projects'
       initial='offscreen'
       whileInView='onscreen'
-      viewport={{ once: true, amount: 0.5 }}
+      viewport={{ once: true, amount: 0 }}
+      variants={cardVariants}
     >
-      <motion.div variants={cardVariants}>
-        <GradientBox className='relative p-0'>
-          <Button
-            className='group absolute left-8 top-8 p-3 opacity-0 transition-all duration-500 ease-in-out group-hover:-rotate-45 group-hover:scale-110 group-hover:opacity-100'
-            rel='noreferrer'
-            target='_blank'
-            icon={
-              <div className='text-2xl'>
-                <TbArrowRight />
-              </div>
-            }
-          />
-          <Image
-            src={project.image}
-            alt={project.name}
-            className='mx-auto transition-all duration-500 ease-in-out group-hover:scale-110'
-            width={500}
-            height={500}
-          />
-        </GradientBox>
-        <AnimatedText secondText={project.description} className='ml-8 mt-4'>
-          <FlexDiv className='gap-2'>
-            {project.name}
-            <TbArrowRight className='-rotate-45' />
-          </FlexDiv>
-        </AnimatedText>
-      </motion.div>
+      <GradientBox className='relative p-0'>
+        <Button
+          className='group absolute left-8 top-8 p-3 opacity-0 transition-all duration-500 ease-in-out group-hover:-rotate-45 group-hover:scale-110 group-hover:opacity-100'
+          rel='noreferrer'
+          target='_blank'
+          icon={
+            <div className='text-2xl'>
+              <TbArrowRight />
+            </div>
+          }
+        />
+        <Image
+          src={project.image}
+          alt={project.name}
+          className='mx-auto transition-all duration-500 ease-in-out group-hover:scale-110'
+          width={500}
+          height={500}
+        />
+      </GradientBox>
+      <AnimatedText secondText={project.description} className='ml-8 mt-4'>
+        <FlexDiv className='gap-2'>
+          {project.name}
+          <TbArrowRight className='-rotate-45' />
+        </FlexDiv>
+      </AnimatedText>
     </motion.section>
   );
 }
