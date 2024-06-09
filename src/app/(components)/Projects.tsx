07a6bd44f019fb989ef14471a5c82e7ae959ae56
Kacper Wojak <kacperwojak17@gmail.com>
2024-06-09 09:43:29 +0200

    HomePage: Add animation for projects grid

--- src/app/(components)/Projects.tsx
+++ src/app/(components)/Projects.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { motion, Variants } from 'framer-motion';
 import Image from 'next/image';
 import React from 'react';
 import { TbArrowRight } from 'react-icons/tb';
@@ -7,6 +10,23 @@ import Button from '../components/Button';
 import FlexDiv from '../components/FlexDiv';
 import GradientBox from '../components/GradientBox';
 import { Project, projects } from '../utils/data';
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 200,
+    opacity: 0,
+    scale: 0.7,
+  },
+  onscreen: {
+    y: 0,
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.8,
+    },
+  },
+};
 
 export default function Projects() {
   return (
@@ -24,32 +44,40 @@ interface ProjectCardProps {
 
 function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <section className='group cursor-pointer' id='projects'>
-      <GradientBox className='relative p-0'>
-        <Button
-          className='group absolute left-8 top-8 p-3 opacity-0 transition-all duration-500 ease-in-out group-hover:-rotate-45 group-hover:scale-110 group-hover:opacity-100'
-          rel='noreferrer'
-          target='_blank'
-          icon={
-            <div className='text-2xl'>
-              <TbArrowRight />
-            </div>
-          }
-        />
-        <Image
-          src={project.image}
-          alt={project.name}
-          className='mx-auto transition-all duration-500 ease-in-out group-hover:scale-110'
-          width={500}
-          height={500}
-        />
-      </GradientBox>
-      <AnimatedText secondText={project.description} className='ml-8 mt-4'>
-        <FlexDiv className='gap-2'>
-          {project.name}
-          <TbArrowRight className='-rotate-45' />
-        </FlexDiv>
-      </AnimatedText>
-    </section>
+    <motion.section
+      className='group cursor-pointer'
+      id='projects'
+      initial='offscreen'
+      whileInView='onscreen'
+      viewport={{ once: true, amount: 0.5 }}
+    >
+      <motion.div variants={cardVariants}>
+        <GradientBox className='relative p-0'>
+          <Button
+            className='group absolute left-8 top-8 p-3 opacity-0 transition-all duration-500 ease-in-out group-hover:-rotate-45 group-hover:scale-110 group-hover:opacity-100'
+            rel='noreferrer'
+            target='_blank'
+            icon={
+              <div className='text-2xl'>
+                <TbArrowRight />
+              </div>
+            }
+          />
+          <Image
+            src={project.image}
+            alt={project.name}
+            className='mx-auto transition-all duration-500 ease-in-out group-hover:scale-110'
+            width={500}
+            height={500}
+          />
+        </GradientBox>
+        <AnimatedText secondText={project.description} className='ml-8 mt-4'>
+          <FlexDiv className='gap-2'>
+            {project.name}
+            <TbArrowRight className='-rotate-45' />
+          </FlexDiv>
+        </AnimatedText>
+      </motion.div>
+    </motion.section>
   );
 }
